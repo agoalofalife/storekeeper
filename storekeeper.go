57d21576aa7a209ed584067ad12c24215c63f747
agoalofalife <agoalofalife@gmail.com>
2017-05-27 13:29:03 +0300

    feel for the idea

--- storekeeper.go
+++ storekeeper.go
@@ -1,17 +1,27 @@
 package storekeeper
 
 type store struct {
-  instance map[interface{}]interface{}
-  binding  map[interface{}]interface{}
-
+	instance map[interface{}]interface{}
+	binding  map[interface{}]interface{}
 }
+
 // Initialization or create
 func New() *store {
-  storage := &store{}
-  return storage
+	storage := &store{}
+	storage.instance = make(map[interface{}]interface{})
+	storage.binding = make(map[interface{}]interface{})
+	return storage
 }
 
-func (store *store) SetInstance(abstract interface{}, instance interface{}) {
-
+func (store *store) SetInstance(abstract interface{}, instance interface{}) *store {
+	store.instance[abstract] = instance
+	return store
 }
 
+// extract from storage
+func (store *store) Extract(abstract interface{}) interface{} {
+	if instance, exist := store.instance[abstract]; exist {
+		return instance
+	}
+	return nil
+}
