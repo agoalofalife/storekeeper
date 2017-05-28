3e84d93ac1622b2741b71cc4aa068ec1f70d7fbb
agoalofalife <agoalofalife@gmail.com>
2017-05-28 11:38:59 +0300

    closure functional in binding

--- storekeeper.go
+++ storekeeper.go
@@ -1,5 +1,12 @@
 package storekeeper
 
+import (
+	"errors"
+	"log"
+	"os"
+	"reflect"
+)
+
 type store struct {
 	instance map[interface{}]interface{}
 	binding  map[interface{}]interface{}
@@ -23,5 +30,30 @@ func (store *store) Extract(abstract interface{}) interface{} {
 	if instance, exist := store.instance[abstract]; exist {
 		return instance
 	}
+	if _, exist := store.binding[abstract]; exist {
+		values, _ := store.call(store.binding, `name`)
+		log.Println(values[0])
+		os.Exit(2)
+	}
 	return nil
+}
+
+func (store *store) Bind(abstract interface{}, concrete interface{}) *store {
+	store.binding[abstract] = concrete
+	return store
+}
+
+// function call closure and return ready structure
+func (store *store) call(m map[interface{}]interface{}, name string, params ...interface{}) (result []reflect.Value, err error) {
+	f := reflect.ValueOf(m[name])
+	if len(params) != f.Type().NumIn() {
+		err = errors.New("The number of params is not adapted.")
+		return
+	}
+	in := make([]reflect.Value, len(params))
+	for k, param := range params {
+		in[k] = reflect.ValueOf(param)
+	}
+	result = f.Call(in)
+	return
 }
